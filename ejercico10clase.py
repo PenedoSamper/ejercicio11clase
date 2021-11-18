25c37a9fa10ad1ce6d91e4cb9f2e6695d190e3ee
Nicolás Penedo Samper <nicopenedos@gmail.com>
2021-11-18 11:30:23 +0100

    Initial commit

---
+++ ejercico10clase.py
@@ -0,0 +1,23 @@
+class Car:
+    """class represent and manage cars"""
+
+    num_cars = 0
+
+    def __init__(self, marca, modelo, combustible, cilindrada):
+        self.marca = marca
+        self.modelo = modelo
+        self.combustible = combustible
+        self.cilindrada = cilindrada
+        self.pos_x = 0
+        self.pos_y = 0
+
+    def move_to(x, y)
+        self.pos_x = x
+        self.pos_y = y
+
+    def move_incr(x, y):
+        self.pos_x += x
+        self.pos_y += y
+
+    def get_pos():
+        return self.pos_x, self.pos:y
---
+++ persistencia_pickle.py
@@ -0,0 +1,14 @@
+import pickle
+
+
+def store(data, filename):
+    pickle.dump(data, open(filename, "wb"))
+def retrieve(filename):
+    try:
+        f_0 = open(filename,"rb")
+    except:
+        print("Error al abrir el archivo", filename)
+        return
+    content = pickle.load(f_0)
+    f_o.cole()
+    return content
